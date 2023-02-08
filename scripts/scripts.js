c27954d70b5652c56ec5b8139c410504502f3656
shrotia <119420493+shrotia@users.noreply.github.com>
2023-02-08 15:19:40 +0530

    Added embed block & Polaris integration

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,6 +13,12 @@ import {
   loadCSS,
 } from './lib-franklin.js';
 
+
+import {
+  decoratePolarisAssets
+} from './lib-polaris.js'
+
+
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information here
 
@@ -52,6 +58,7 @@ export function decorateMain(main) {
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
+  decoratePolarisAssets(main);
 }
 
 /**
